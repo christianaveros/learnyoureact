6aaef543bea656eff5d9067fdb58d0178fb3a502
Christian <christianaveros@gmail.com>
2017-07-26 12:25:45 +0800

    Added in css and state problems

--- views/index.jsx
+++ views/index.jsx
@@ -19,8 +19,9 @@ class TodoList extends React.Component {
 			<div className="todoList">
 				<table style={{border: "2px solid black"}}>
 					<tbody>
-                    <Todo title="Shopping">Milk</Todo>
-                    <Todo title="Hair Cut">13:00</Todo>
+                        <Todo title="Shopping">Milk</Todo>
+                        <Todo title="Hair cut">13:00</Todo>
+                        <Todo title="Learn React">15:00</Todo>
 					</tbody>
 				</table>
 			</div>
@@ -29,17 +30,31 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
-	render () {
-		return (
-			<div className="todo">
-                <tr>
-                    <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                    <td style={{border: "1px solid black"}}>{this.props.children}</td>
-                </tr>
-			</div>
-		);
-	}
+    constructor(props){
+        super(props);
+        this.state = {checked: false};
+    }
+
+    handleChange(e) {
+        this.setState({checked: e.target.checked});
+    }
+
+    render () {
+	return (
+            <tr>
+                <td style={style.tableContent}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td style={style.tableContent}>{this.props.title}</td>
+                <td style={style.tableContent}>{this.props.children}</td>
+            </tr>
+	);
+    }
 }
+
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
 
 class TodoForm extends React.Component {
 	render () {
@@ -50,3 +65,9 @@ class TodoForm extends React.Component {
 		);
 	}
 }
+
+let style = {
+	tableContent: {
+		border: "1px solid black"
+	}
+};
